3f2eb5265df1b7c9a989f6d14e475284a97d53c3
yulheon <smgr8178@naver.com>
2021-05-01 18:02:52 +0900

    행사에 맞게 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import eventImage from "./images/event_20210501.png";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img
+        src={eventImage}
+        className=""
+        alt="event"
+        style={{ width: "100%", maxWidth: "1000px" }}
+      />
     </div>
   );
 }
